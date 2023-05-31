4c27a6fe0a01592039304efe2c6c8ebf85fef8b6
Victor Donizete <victordonizete65@gmail.com>
2023-05-31 16:48:05 -0300

    global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,6 +11,19 @@ export const GlobalStyles = createGlobalStyle`
         max-width: 1120px;
         margin: auto;
 
+        &::-webkit-scrollbar {
+            width: 8px; /* Largura da barra de rolagem */
+        }
+
+        &::-webkit-scrollbar-track {
+            background-color: transparent; /* Cor do fundo da barra de rolagem */
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background: linear-gradient(270deg, rgba(19, 173, 199, 0.5), rgba(105, 120, 209, 0.5)); /* Cor da thumb (alça) da barra de rolagem */
+            border-radius: 20px;
+        }
+
         button {
             font-family: 'Poppins', sans-serif;
         }
