b41857e2a928e134c6864b8d3f6f800f858aed6c
Victor Donizete <victordonizete65@gmail.com>
2023-05-30 23:13:33 -0300

    global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Poppins', sans-serif;
+
+        background-color: #0F1624;
+
+        max-width: 1120px;
+        margin: auto;
+
+        button {
+            font-family: 'Poppins', sans-serif;
+        }
+    }
+`
