c264821609754d13ca4db3e1973e218a582475d0
Victor Donizete <victordonizete65@gmail.com>
2023-05-31 10:06:42 -0300

    remove import from teste.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import './teste.css'
-
-// import teste from './mais.svg'
 import { GlobalStyles } from './styles/global'
 import { Header } from './components/header'
 import { Summary } from './components/summary'
